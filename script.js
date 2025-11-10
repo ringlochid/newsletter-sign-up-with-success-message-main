7fe14a7418b9c5545c203c8d8b4019c330e2baa3
Leo Zhang <ring_lochid@outlook.com>
2025-11-11 00:35:55 +1100

    v0.4

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function main(){
     const form_sign_in = document.getElementById("sign_in_form");
+    const emailInput = document.getElementById('email');
+    const confirmEmail = document.getElementById('confirm-email');
     const btn_dismiss_suc_msg = document.getElementById("dismiss_suc_msg");
     const sign_in_panel = document.getElementById("sign-in-panel");
     const success_panel = document.getElementById("succeed-panel");
@@ -7,13 +9,17 @@ function main(){
     function handle_sign_in(e){
         e.preventDefault();
         const fm = new FormData(form_sign_in);
+        const email = fm.get('email');
+        confirmEmail.textContent = email;
         sign_in_panel.classList.toggle('is-open', false);
         success_panel.classList.toggle('is-succeed', true);
     }
 
-    function dismiss_succeed_msg(e){
+    function dismiss_succeed_msg(){
         success_panel.classList.toggle('is-succeed', false);
         sign_in_panel.classList.toggle('is-open', true);
+        form_sign_in.reset();
+        emailInput.focus();
     }
 
     form_sign_in.addEventListener('submit', handle_sign_in);
