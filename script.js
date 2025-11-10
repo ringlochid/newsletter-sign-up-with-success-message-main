ace8a12a0fb1769460371dfeed166a87e5c30f51
Leo Zhang <ring_lochid@outlook.com>
2025-11-11 00:06:26 +1100

    v0.2

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function main(){
+    const form_sign_in = document.getElementById("sign_in_form");
+    const sign_in_panel = document.getElementById("sign-in-panel");
+    
+    function handle_sign_in(e){
+        e.preventDefault();
+        const fm = new FormData(form_sign_in)
+        sign_in_panel.classList.toggle('is-open', false)
+    }
+
+    form_sign_in.addEventListener('submit', handle_sign_in);
 
 }
 
